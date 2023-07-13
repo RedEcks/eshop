4949527a3c8118ec5f10c845fce15c2b9a9ad988
Xavier Dunkley <xavierxv18@gmail.com>
2023-07-13 09:22:27 -0400

    header component almost done

--- src/Header.js
+++ src/Header.js
@@ -5,26 +5,23 @@ function Header(){
 
     return (
         <div className="header">
-            <div className='header_logo'>
-                <img src="https://media.istockphoto.com/id/1189778189/vector/grandma-or-granny-mascot-character-cartoon-design-vector-format.jpg?s=612x612&w=0&k=20&c=tsQ6g2tZ325WP26Qyx_XberGvr3qpADeUTJJPPmCydM=" className='header_logo' alt="" />
+            <div className='header-logo'>
+                <img src="https://media.istockphoto.com/id/1189778189/vector/grandma-or-granny-mascot-character-cartoon-design-vector-format.jpg?s=612x612&w=0&k=20&c=tsQ6g2tZ325WP26Qyx_XberGvr3qpADeUTJJPPmCydM=" className='header-logo' width="200" height="200" alt="" />
                 <h3 className='header-title'>Grandma's Bakery</h3>
             </div>
-            <div className='header_search'>
+            <div className='header-search'>
                 <input type='text' className='header-search'/>
-                <img src="https://cdn-icons-png.flaticon.com/512/954/954591.png" className='header-search-icon' alt=''/>
+                <img src="https://cdn-icons-png.flaticon.com/512/954/954591.png" className='header-search-icon' width="20" height="20" alt=''/>
             </div>
-            <div className='header_nav'>
+            <div className='header-nav'>
                 <div className='nav_item'>
-                    <span className='nav_itemLineOne'>Hello Guest</span>
-                    <span className='nav_itemLineTwo'>Sign In</span>
+                    <span className='guest-greeting'>Hello Guest</span>
+                    <span className='sign-in'>Sign In</span>
                 </div>
-                <div className='nav_item'>
-                    <span className='nav_itemLineOne'>Your</span>
-                    <span className='nav_itemLineTwo'>Shop</span>
-                </div>
-                <div className='nav_item'>
-                    <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" className='shopping-cart-logo' alt="" />
-                    <span className='nav_itemLineTwo'>0</span>
+                <div className='nav-item'>
+                    <span className='shopping-cart'>Your Cart</span>
+                    <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" className='shopping-cart-logo' width="35" height="35" alt="" />
+                    <span className='nav-itemNumber'>0</span>
                 </div>
             </div>
         </div>
