dd3b9fef975d1179eb57e9abf6a49ef12d9798af
Xavier Dunkley <xavierxv18@gmail.com>
2023-07-14 10:37:14 -0400

    added home page background image

--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,10 @@ function Home(){
 
     return(
         <div>
-            <div className="Home">
-                
+            <div className="home">
+                <div className="home-container">
+                    <img src="https://img.freepik.com/free-photo/frame-food-ingredients-baking-gently-pink-pastel-background-cooking-flat-lay-with-copy-space-top-view-baking-concept-flat-lay_127032-2200.jpg" alt="" className="home-image" width="1519" height="1000"/>
+                </div>
             </div>
         </div>
     )
