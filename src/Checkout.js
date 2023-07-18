b34b72e8c549c704edf967ab68701bb0c0a82ddc
Xavier Dunkley <xavierxv18@gmail.com>
2023-07-17 21:02:57 -0400

    added Add link for checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,11 @@
 import React from "react"
+import "./Checkout.css"
 
 function Checkout(){
 
     return(
         <div>
-
+            <h1>Hello World</h1>
         </div>
     )
 }
--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,18 @@
 import React from "react";
-import "./Header.css"
+import "./Header.css";
+import { Link, BrowserRouter } from "react-router-dom";
 
 function Header(){
 
     return (
+        <BrowserRouter>
         <div className="header">
-            <div className='header-logo'>
-                <img src="https://media.istockphoto.com/id/1189778189/vector/grandma-or-granny-mascot-character-cartoon-design-vector-format.jpg?s=612x612&w=0&k=20&c=tsQ6g2tZ325WP26Qyx_XberGvr3qpADeUTJJPPmCydM=" className='header-logo' width="189" height="200" alt="" />
-                <h3 className='header-title'>Grandma's Bakery</h3>
-            </div>
+            <Link to='/'>
+                <div className='header-logo'>
+                    <img src="https://media.istockphoto.com/id/1189778189/vector/grandma-or-granny-mascot-character-cartoon-design-vector-format.jpg?s=612x612&w=0&k=20&c=tsQ6g2tZ325WP26Qyx_XberGvr3qpADeUTJJPPmCydM=" className='header-logo' width="189" height="200" alt="" />
+                    <h3 className='header-title'>Grandma's Bakery</h3>
+                </div>
+            </Link>
             <div className='header-search'>
                 <input type='text' className='search'/>
                 <img src="https://cdn-icons-png.flaticon.com/512/954/954591.png" className='header-search-icon' width="20" height="20" alt=''/>
@@ -18,13 +22,19 @@ function Header(){
                     <span className='guest-greeting'>Hello Guest</span>
                     <span className='sign-in'>Sign In</span>
                 </div>
-                <div className='nav-item'>
-                    <span className='shopping-cart'>Your Cart</span>
-                    <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" className='shopping-cart-logo' width="25" height="25" alt="" />
-                    <span className='nav-itemNumber'>0</span>
-                </div>
+                <nav>
+
+                    <Link to="/checkout" forceRefresh={true}>
+                        <div className='nav-item'>
+                            <span className='shopping-cart'>Your Cart</span>
+                            <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" className='shopping-cart-logo' width="25" height="25" alt="" />
+                            <span className='nav-itemNumber'>0</span>
+                        </div>
+                    </Link>
+                </nav>
             </div>
         </div>
+        </BrowserRouter>
     )
 
 
